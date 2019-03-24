823a86925e6e56c35afcbc345530197d78b6c7b4
leslie <lescold@gmail.com>
2019-03-24 07:49:51 -0400

    create filter system

--- src/main/java/com/stickersthecat/brew/motd/Main.java
+++ src/main/java/com/stickersthecat/brew/motd/Main.java
@@ -1,5 +1,8 @@
 package com.stickersthecat.brew.motd;
 
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+
 import com.stickersthecat.brew.motd.events.PingEvent;
 
 import org.bukkit.configuration.file.FileConfiguration;
@@ -8,7 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     public FileConfiguration Config;
-    
+
     @Override
     public void onEnable() {
 
@@ -17,5 +20,19 @@ public class Main extends JavaPlugin {
         this.Config = this.getConfig();
 
         this.getServer().getPluginManager().registerEvents(new PingEvent(this), this);
+    }
+
+    public String StringFilter(String Old) {
+
+        String New = Old;
+
+        // do time
+        int Hour = ZonedDateTime.now().getHour();
+        String HourS = String.format("%1$" + 2 + "s", Hour).replace(' ', '0');
+        int Minute = ZonedDateTime.now().getMinute();
+        String MinuteS = String.format("%1$" + 2 + "s", Minute).replace(' ', '0');
+        New = New.replace("{EARTH_TIME}", HourS + ":" + MinuteS);
+
+        return New;
     }
 }
