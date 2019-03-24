0311f34bfce67b3a287a1af6778124c6c5700e69
leslie <lescold@gmail.com>
2019-03-24 06:42:57 -0400

    add basic time funcs

--- src/main/java/com/stickersthecat/brew/motd/events/PingEvent.java
+++ src/main/java/com/stickersthecat/brew/motd/events/PingEvent.java
@@ -1,5 +1,8 @@
 package com.stickersthecat.brew.motd.events;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -23,7 +26,7 @@ public class PingEvent implements Listener {
     public void OnPing(ServerListPingEvent Event) {
 
         // make sure MOTD exists
-        if (this.Plugin.Config.contains("motd")) 
+        if (this.Plugin.Config.contains("motd"))
             this.MOTD(Event);
     }
 
@@ -35,13 +38,16 @@ public class PingEvent implements Listener {
         if (!this.Plugin.Config.contains("motd.mode"))
             return;
 
-        switch (this.Plugin.Config.getString("motd.mode")) {
+        switch (this.Plugin.Config.getString("motd.mode").toLowerCase()) {
 
         case "static":
             MOTD = this.StaticMOTD();
             break;
         case "random":
             MOTD = this.RandomMOTD();
+            break;
+        case "earthtime":
+            MOTD = this.EarthTimeMOTD();
             break;
         default:
             break;
@@ -50,7 +56,6 @@ public class PingEvent implements Listener {
         // if fail use static
         if (MOTD == "" && this.Plugin.Config.contains("motd.static"))
             MOTD = this.BuildMOTD(this.Plugin.Config.getStringList("motd.static"));
-
 
         // check it again and if this fails allow it to default to the server prop one
         if (MOTD != "")
@@ -63,19 +68,6 @@ public class PingEvent implements Listener {
     private String StaticMOTD() {
 
         return this.StaticKey("motd.static");
-    }
-
-    /**
-     * Root Level random MOTD
-     */
-    private String RandomMOTD() {
-
-        return this.RandomKey("motd.random");
-    }
-
-    private String EarthTimeMOTD() {
-
-        return "";
     }
 
     /**
@@ -94,6 +86,14 @@ public class PingEvent implements Listener {
 
         // now build it
         return this.BuildMOTD(this.Plugin.Config.getStringList(Key));
+    }
+
+    /**
+     * Root Level random MOTD
+     */
+    private String RandomMOTD() {
+
+        return this.RandomKey("motd.random");
     }
 
     /**
@@ -124,6 +124,130 @@ public class PingEvent implements Listener {
             MOTD = this.BuildMOTD(this.Plugin.Config.getStringList(Selected));
 
         return MOTD;
+    }
+
+    private String EarthTimeMOTD() {
+
+        String MOTD = "";
+
+        // start this one off by making sure the time key exists
+        if (!this.Plugin.Config.contains("motd.earthtime"))
+            return MOTD;
+
+        // now get current time frame
+        String Frame = this.EarthTimeSelect();
+
+        // if empty fail
+        if (Frame == "")
+            return this.EarthTimeDefault();
+
+        // if no mode is set fail
+        if (!this.Plugin.Config.contains(Frame + ".mode"))
+            return this.EarthTimeDefault();
+
+        switch (this.Plugin.Config.getString(Frame + ".mode").toLowerCase()) {
+
+        case "random":
+            MOTD = this.RandomKey(Frame + ".random");
+            break;
+        case "static":
+            MOTD = this.StaticKey(Frame + ".static");
+            break;
+        }
+
+        if (MOTD == "")
+            MOTD = this.EarthTimeDefault();
+
+        return MOTD;
+    }
+
+    /**
+     * Loop throw the time until we find one that matches then return full key
+     */
+    private String EarthTimeSelect() {
+
+        String Key = "";
+        Set<String> Frames = this.Plugin.Config.getConfigurationSection("motd.earthtime.frames").getKeys(false);
+
+        // make sure list isnt empty
+        if (Frames.isEmpty())
+            return Key;
+
+        for (String Value : Frames) {
+
+            // make sure both time values are set
+            if (!this.Plugin.Config.contains("motd.earthtime.frames." + Value + ".from")
+                    && !this.Plugin.Config.contains("motd.earthtime.frames." + Value + ".till"))
+                return Key;
+
+            // next do the time compares
+            String TimeFrom = this.Plugin.Config.getString("motd.earthtime.frames." + Value + ".from");
+            String TimeTo = this.Plugin.Config.getString("motd.earthtime.frames." + Value + ".till");
+
+            if (this.timeCompare(TimeFrom, true) && this.timeCompare(TimeTo, false)) {
+
+                Key = "motd.earthtime.frames." + Value;
+                break;
+            }
+        }
+
+        return Key;
+    }
+
+    private String EarthTimeDefault() {
+
+        String MOTD = "";
+
+        // make sure exists
+        if (this.Plugin.Config.contains("motd.earthtime.default"))
+            return MOTD;
+
+        switch (this.Plugin.Config.getString("motd.earthtime.default").toLowerCase()) {
+
+        case "random":
+            MOTD = this.RandomMOTD();
+            break;
+        case "static":
+            MOTD = this.StaticMOTD();
+            break;
+        }
+
+        return MOTD;
+    }
+
+    private boolean timeCompare(String Time, boolean Greater) {
+
+        try {
+
+            SimpleDateFormat Format = new SimpleDateFormat("HH:mm");
+            Date FrameTime = Format.parse(Time);
+            Date TimeNow = new Date();
+
+            // if greater, see if time is after
+            if (Greater == true) {
+
+                if (FrameTime.after(TimeNow)) {
+
+                    return true;
+                } else if (FrameTime.equals(TimeNow)) {
+
+                    return true;
+                }
+            } else { // assume greater is false, run before
+
+                if (FrameTime.before(TimeNow)) {
+
+                    return true;
+                } else if (FrameTime.equals(TimeNow)) {
+
+                    return true;
+                }
+            }
+        } catch (ParseException e) {
+
+        }
+
+        return false;
     }
 
     public String BuildMOTD(List<String> Values) {
